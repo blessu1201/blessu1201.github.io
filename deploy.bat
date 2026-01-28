153cc8801ac598f82a1b5dc8599e894619680e88
blessu1201 <blessu1201@gmail.com>
2026-01-28 11:01:18 +0900

    'update'

--- deploy.bat
+++ deploy.bat
@@ -2,11 +2,11 @@
 :: Git 저장소의 변경 사항을 커밋하고 푸시합니다.
 @echo off
 
-echo update 된 항목 추가
+:: update 된 항목 추가
 git add -A
 
-echo commit & description
+:: commit & description
 git commit -m 'update'
 
-echo 배포
+:: 배포
 git push
